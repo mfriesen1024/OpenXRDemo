a2173e4b15ccc3ddca3b6f3335e22587beb2ae40
mFriesen <mfriesen1024@gmail.com>
2026-02-28 17:50:23 -0400

    initial commit

---
+++ Scripts/GDScript/GM.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var root:Window = get_parent();
+	
+	root.use_xr=true;
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
