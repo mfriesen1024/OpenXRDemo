00d486193fc2cc647755d61f55c6470a16fee374
mFriesen <mfriesen1024@gmail.com>
2026-02-28 19:36:34 -0400

    Added spawner

---
+++ Scripts/Dotnet/src/Core/Spawner.cs
@@ -0,0 +1,17 @@
+using Godot;
+
+namespace CloneSaber.Scripts.Dotnet.Core;
+
+public partial class Spawner:Node3D
+{
+    [Export] PackedScene[] objects;
+    [Export] byte objectIndex;
+    [Export] Vector3 position;
+
+    void Spawn()
+    {
+        var node = objects[objectIndex].Instantiate() as Node3D;
+        AddChild(node);
+        node.Position = position;
+    }
+}
