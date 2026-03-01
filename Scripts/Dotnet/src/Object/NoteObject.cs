3d26303b498c2367597dd8f590b8e117420438d0
mFriesen <mfriesen1024@gmail.com>
2026-02-28 20:05:24 -0400

    It helps to move notes.

--- Scripts/Dotnet/src/Object/NoteObject.cs
+++ Scripts/Dotnet/src/Object/NoteObject.cs
@@ -4,7 +4,19 @@ namespace CloneSaber.Scripts.Dotnet.Object;
 
 public abstract partial class NoteObject:Node3D
 {
+    double timeAlive;
     public abstract void Slash(byte saberId);
 
     public abstract void Bonk();
+
+    public override void _PhysicsProcess(double delta)
+    {
+        timeAlive += delta;
+
+        if (timeAlive > 1) return;
+        var tPos = Position;
+        tPos.Z = -(GetParent() as Node3D).Position.Z;
+
+        Position = Position.Lerp(tPos,(float)(timeAlive * 1 / 0.6));
+    }
 }
