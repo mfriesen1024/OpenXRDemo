fee94053eb1d0874bc686bc0838d1c0f8d1dab69
mFriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2026-03-03 10:01:05 -0400

    Prefab-ified UI, world and music player.

--- Scripts/Dotnet/src/Core/GameManager.cs
+++ Scripts/Dotnet/src/Core/GameManager.cs
@@ -1,5 +1,6 @@
 // Copyright © 2026 ScottishDwarfStudios under licence from mFriesen1024 (mfriesen1024@gmail.com)
 
+using CloneSaber.Scripts.Dotnet.Core;
 using Godot;
 
 namespace CloneSaber.Scripts.Dotnet.Object;
@@ -8,11 +9,27 @@ internal partial class GameManager:Node
 {
     public static GameManager Instance { get; set; }
     
-    [Export] PackedScene WorldScene;
+    [Export] PackedScene WorldScene, MusicPrefab;
+    [Export] XRCamera3D Camera;
     [Export] public double ApproachRate = 0.9;
+
+    double timeAlive;
+    bool spawned = false;
 
     public override void _Ready()
     {
         Instance = this;
+
+        var world = WorldScene.Instantiate() as Node3D;
+        world.GlobalBasis = Camera.GlobalBasis;
+        world.GlobalPosition = Camera.GlobalPosition;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        timeAlive += delta;
+        if (timeAlive < ApproachRate) return;
+        if (spawned) return;
+        spawned = true;
     }
 }
