fb8c27a4ae3739ede728247cd0ea6465389525f3
mFriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2026-03-03 11:03:46 -0400

    Update initializer.

--- Scripts/Dotnet/src/Core/GameManager.cs
+++ Scripts/Dotnet/src/Core/GameManager.cs
@@ -14,17 +14,14 @@ internal partial class GameManager:Node
     [Export] public double ApproachRate = 0.9;
 
     double timeAlive;
-    bool spawned = false;
+    bool spawnedWorld;
+    bool spawnedMusic;
 
     public override void _Ready()
     {
         Instance = this;
 
         EventSystem.ScoreUpdated += EventSystemOnScoreUpdated;
-
-        var world = WorldScene.Instantiate() as Node3D;
-        Camera.AddChild(world);
-        world.Reparent(this);
     }
 
     void EventSystemOnScoreUpdated(Vector3 pos, bool isPositive)
@@ -38,9 +35,30 @@ internal partial class GameManager:Node
     public override void _PhysicsProcess(double delta)
     {
         timeAlive += delta;
+        WorldSpawner(delta);
+        MusicSpawner(delta);
+    }
+
+    void WorldSpawner(double delta)
+    {
+        if (spawnedWorld) return;
+        if(timeAlive < 1) return;
+        spawnedWorld = true;
+        
+        var world = WorldScene.Instantiate() as Node3D;
+        AddChild(world);
+        
+        world.GlobalPosition = Camera.GlobalPosition;
+        world.GlobalBasis = Camera.GlobalBasis;
+        
+        timeAlive = 0;
+    }
+
+    void MusicSpawner(double delta)
+    {
         if (timeAlive < ApproachRate) return;
-        if (spawned) return;
-        spawned = true;
+        if (spawnedMusic) return;
+        spawnedMusic = true;
         AddChild(MusicPrefab.Instantiate());
     }
 }
